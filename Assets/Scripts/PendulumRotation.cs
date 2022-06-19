387c283184267fae8a694a0b47b7f18f62b95a9a
Donggyu Kim <31647710+Dogy-K@users.noreply.github.com>
2022-06-20 02:26:27 +0900

    마무리 2차

--- Assets/Scripts/PendulumRotation.cs
+++ Assets/Scripts/PendulumRotation.cs
@@ -13,7 +13,7 @@ public class PendulumRotation : MonoBehaviour
 	void Awake()
 	{
 		if (randomStart)
-			random = Random.Range(0f, 2f);
+			random = Random.Range(0f, 3f);
 	}
 
 	// Update is called once per frame
